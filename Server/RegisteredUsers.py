a3ebb2cce922270e59fd3371201000423e7dfe9d
Sophie Mori <ksmori14@gmail.com>
2017-05-01 18:11:01 +0200

    Add eli and sophie as users

--- Server/RegisteredUsers.py
+++ Server/RegisteredUsers.py
@@ -1,5 +1,7 @@
 RegisteredUsers = [
     {"user_name": "Elon", "password": "Musk"},
     {"user_name": "Bill", "password": "Gates"},
-    {"user_name": "Steve", "password": "Jobs"}
+    {"user_name": "Steve", "password": "Jobs"},
+    {"user_name": "Eli", "password": "Eli"},
+    {"user_name": "Sofi", "password": "avocado"}
 ]
